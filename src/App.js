7842d117d860f6ea29fd0ec3a97cca8d38cabbe9
Shelby-Jean <shelby.gottschall@gmail.com>
2020-11-27 12:59:20 -0500

    single date search added

--- src/App.js
+++ src/App.js
@@ -4,29 +4,21 @@ import Stories from './components/Stories';
 import './App.css';
 
 class App extends React.Component {
-
-  constructor() {
-    super()
-      this.state = {
-        // stories: [],
+  state = {
         keywordInput: '',
         authorInput: '',
+        dateInput: '',
+        dateInSeconds: '',
         relatedStories: []
-    }
   }
 
-  // componentDidMount = () => {
-  //   fetch('http://hn.algolia.com/api/v1/search?query=foo&tags=story')
-  //     .then(response => response.json())
-  //     .then(data => this.setState({
-  //       stories: data.hits
-  //     }))
-  //     .catch(error => console.log(`Error, ${error}`))
-  // }
 
-  onChange = (event) => {
+  handleChange = (event) => {
+    const epochDate = new Date(event.target.value).getTime();
+    const searchDate = epochDate.toString().slice(0,10);
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      dateInSeconds: searchDate
     })
   }
 
@@ -34,6 +26,7 @@ class App extends React.Component {
     event.preventDefault();
     const keyword = this.state.keywordInput;
     const author = this.state.authorInput;
+    const date = this.state.dateInSeconds;
     let url = '';
 
     if(keyword && author) {
@@ -42,6 +35,8 @@ class App extends React.Component {
       url = `http://hn.algolia.com/api/v1/search?query=${keyword}&tags=story`;
     } else if(author) {
       url = `https://hn.algolia.com/api/v1/search?tags=story,author_${author}`;
+    } else if(date) {
+      url=`http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=created_at_i>${date}`
     } else {
       alert("Please enter search criteria in at least one input");
     }
@@ -51,7 +46,7 @@ class App extends React.Component {
         .then(data => this.setState({
           relatedStories: data.hits,
           keywordInput: '',
-          authorInput: ''
+          authorInput: '',
         }))
         .catch(error => console.log(`Error, ${error}`))
   }
@@ -62,7 +57,13 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>News Feed</h1>
-          <SearchForm search={this.search} onChange={this.onChange} keywordInput={this.state.keywordInput} authorInput={this.state.authorInput} />
+          <SearchForm 
+            search={this.search} 
+            handleChange={this.handleChange} 
+            keywordInput={this.state.keywordInput} 
+            authorInput={this.state.authorInput} 
+            dateInput={this.state.dateInput} 
+          />
           <Stories relatedStories={this.state.relatedStories} />
         </header>
       </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,15 +6,23 @@ const SearchForm = (props) => {
       <form onSubmit={props.search}>
         <input 
           name="keywordInput"
+          type="text"
           value={props.keywordInput} 
-          onChange={props.onChange} 
+          onChange={props.handleChange} 
           placeholder="Search by keyword"
         />
         <input 
           name="authorInput"
+          type="text"
           value={props.authorInput} 
-          onChange={props.onChange} 
+          onChange={props.handleChange} 
           placeholder="Search by author"
+        />
+        <input 
+          name="dateInput"
+          type="date"
+          value={props.dateInput} 
+          onChange={props.handleChange}
         />
         <button type="submit">Search</button>
       </form>
